79c5497b7e159b2ce7a7f5aa79d511c9a137f95d
IlyaZakrevsky <cudinpack95@yandex.ru>
2016-07-25 11:54:28 +0300

    FileManager_CopyFile

--- eltex/curs/tfm/tfm.c
+++ eltex/curs/tfm/tfm.c
@@ -2,20 +2,24 @@
 
 int main()
 {
+        WINDOW *key_info;
         TREE_WIN twin_left;
         TREE_WIN twin_right;
         TREE_WIN *now_win;
         int key;
-        int b_l_row;
+        int twin_left_width;
         
         initscr();
         keypad(stdscr, 1);
         noecho();
         refresh();
-        b_l_row = InitWindowTree(&twin_left, 0, 0);
+        key_info = derwin(stdscr, 0, 0, 0, 0);
+        wprintw(key_info, " F10 - Exit    F5 - Copy selected file");
+        wrefresh(key_info);
+        twin_left_width = InitWindowTree(&twin_left, 0, 1);
         CreateDInfoList(&twin_left);
         PrintDInfoList(&twin_left, 1);
-        InitWindowTree(&twin_right, b_l_row, 0);
+        InitWindowTree(&twin_right, twin_left_width, 1);
         CreateDInfoList(&twin_right);
         PrintDInfoList(&twin_right, 1);
         now_win = &twin_left;
@@ -24,6 +28,9 @@ int main()
         while( (key = getch()) > 0 ){
                 if(key == KEY_F(10))
                         break;
+                if(key == KEY_F(5)){
+                        InitCopyFile(&now_win, now_win == &twin_left ? &twin_right : &twin_left);
+                }
                 if(key == KEY_DOWN)
                         NextLine(now_win);
                 if(key == KEY_UP)
@@ -32,7 +39,8 @@ int main()
                         OnPressEnter(now_win);
                 if(key == 9){//TAB
                         now_win = SwitchWin(now_win, &twin_left, &twin_right);
-                        MoveCursorToRowCol(now_win, now_win->cursor.x_row, now_win->cursor.y_col);
+                        ChDir_NotMove(now_win, now_win->cur_dir);
+                        MoveCursorToRowCol(now_win, now_win->cursor.row, now_win->cursor.col);
                 }
         }
         endwin();
@@ -118,7 +126,9 @@ int InitWindowTree(TREE_WIN *win, int x_col, int y_row)
         win->name_list_d_size = 0;
         win->name_index = 1;
 
-        win->cursor.x_row = win->cursor.y_col = 0;
+        win->cursor.row = win->cursor.col = 0;
+
+        getcwd(win->cur_dir, CURDIR_SIZE);
 
         return b_col;
 }
@@ -155,23 +165,31 @@ int CreateDInfoList(TREE_WIN *twin)
 
 void MakeName(char *str, struct stat *sb, char *name)
 {
+        char r = ' ';
+        char w = ' ';
+        char x = ' ';
+        
         if(sb == NULL){
-                sprintf(str, "? %s", name);
-                
+                sprintf(str, "??? %s", name);//Неизвестный
                 return;
         }
         
-        switch(sb->st_mode & S_IFMT){
-                case S_IFDIR:
-                        sprintf(str, "/ %s", name);
-                break;
-                default:
-                        if(sb->st_mode & S_IXUSR || sb->st_mode & S_IXGRP || sb->st_mode & S_IXOTH)
-                                sprintf(str, "* %s", name);
-                        else
-                                sprintf(str, "  %s", name);
-                break;
+        if( (sb->st_mode & S_IFMT) == S_IFDIR ){//Директория
+                sprintf(str, " /  %s", name);
+                return;
         }
+        //Разрешения 
+        if(FileIsExe(sb) == 0){//Запуск
+                x = 'x';
+        }
+        if(FileIsRead(sb) == 0){//Чтение
+                r = 'r';
+        }
+        if(FileIsWrite(sb) == 0){//Запись
+                w = 'w';
+        }
+        
+        sprintf(str, "%c%c%c %s", r, w, x, name);
 }
 
 void PrintDInfoList(TREE_WIN *twin, int start_index)
@@ -203,14 +221,14 @@ void PrintDInfoList(TREE_WIN *twin, int start_index)
         }
         wrefresh(twin->name_list);
         wrefresh(twin->info_list);
-        MoveCursorToRowCol(twin, twin->cursor.x_row, twin->cursor.y_col);
+        MoveCursorToRowCol(twin, twin->cursor.row, twin->cursor.col);
 }
 
 int MoveCursorToRowCol(TREE_WIN *twin, int row, int col)
 {
         if( wmove(twin->name_list, col, row) > -1 ){
-                twin->cursor.x_row = row;
-                twin->cursor.y_col = col;
+                twin->cursor.row = row;
+                twin->cursor.col = col;
                 wrefresh(twin->name_list);
                 return 0;
         }
@@ -220,27 +238,48 @@ int MoveCursorToRowCol(TREE_WIN *twin, int row, int col)
 
 int NextLine(TREE_WIN *twin)
 {
-        int cursor_pos = twin->nl_rows - twin->cursor.y_col;
+        int cursor_pos = twin->nl_rows - twin->cursor.col;
 
         if( (twin->name_index + 1) < twin->name_list_d_size){
                 twin->name_index++;
                 if(cursor_pos == 0)
-                        PrintDInfoList(twin, twin->name_index - twin->cursor.y_col);
+                        PrintDInfoList(twin, twin->name_index - twin->cursor.col);
         } else
                 return -1;
-        return MoveCursorToRowCol(twin, twin->cursor.x_row, twin->cursor.y_col + 1);
+        return MoveCursorToRowCol(twin, twin->cursor.row, twin->cursor.col + 1);
 }
 
 int PrevLine(TREE_WIN *twin)
 {
         if( (twin->name_index - 1) > 0){
                 twin->name_index--;
-                if(twin->cursor.y_col == 0)
+                if(twin->cursor.col == 0)
                         PrintDInfoList(twin, twin->name_index);
         } else
                 return -1;
         
-        return MoveCursorToRowCol(twin, twin->cursor.x_row, twin->cursor.y_col - 1);
+        return MoveCursorToRowCol(twin, twin->cursor.row, twin->cursor.col - 1);
+}
+
+int ChDir(TREE_WIN *twin, char *dname)
+{
+        if(chdir(dname) < 0)
+                return -1;
+        CreateDInfoList(twin);
+        PrintDInfoList(twin, 1);
+        MoveCursorToRowCol(twin, 0, 0);
+        getcwd(twin->cur_dir, CURDIR_SIZE);
+        
+        return 0;
+}
+
+int ChDir_NotMove(TREE_WIN *twin, char *dname)
+{
+        if(chdir(dname) < 0)
+                return -1;
+        getcwd(twin->cur_dir, CURDIR_SIZE);
+        
+        return 0;
 }
 
 int OnPressEnter(TREE_WIN *twin)
@@ -255,13 +294,7 @@ int OnPressEnter(TREE_WIN *twin)
         if(stat(name_list[name_index]->d_name, &stat_buff) > -1){
                 switch(stat_buff.st_mode & S_IFMT){
                         case S_IFDIR:
-                                if( chdir(name_list[name_index]->d_name) < 0 )
-                                        return -1;
-                                CreateDInfoList(twin);
-                                PrintDInfoList(twin, 1);
-                                MoveCursorToRowCol(twin, 0, 0);
-                                
-                                return 0;
+                                return ChDir(twin, name_list[name_index]->d_name);
                         break;
                         default:
                                 if(stat_buff.st_mode & S_IXUSR || stat_buff.st_mode & S_IXGRP || stat_buff.st_mode & S_IXOTH){
@@ -305,11 +338,171 @@ int EditFile(char *fname)
         pid = fork();
         
         if(pid == 0){
-                execlp("medit", "medit", fname, (char*)NULL);
+                execlp("mc", "mc", "-e", fname, (char*)NULL);
         } else {
                 endwin();
                 wait(0);
         }
         
         return 0;
+}
+
+PBAR_WIN *CreatePBAR(TREE_WIN *twin, int x, int y, char *text)
+{
+        PBAR_WIN *pbar;
+        int height, win_h;
+        int width, win_w;
+        
+        GetWinSize(&win_h, &win_w);
+        pbar = malloc(sizeof(PBAR_WIN));
+        if(pbar == NULL)
+                return NULL;
+        height = win_h / 4;
+        width = win_w - 2;
+        if(x < 0 || y < 0){
+                x = 1;
+                y = win_h - height;
+        }
+        pbar->box = newwin(height, width, y, x);
+        box(pbar->box, ' ', '-');
+        wrefresh(pbar->box);
+        pbar->text = derwin(pbar->box, height - 3, width - 2, 1, 1);
+        if(text != NULL)
+                waddstr(pbar->text, text);
+        wrefresh(pbar->text);
+        pbar->progress = derwin(pbar->box, 1, width - 2, height - 2, 1);
+        wrefresh(pbar->progress);
+        pbar->progress_size = width - 2;
+        MoveCursorToRowCol(twin, twin->cursor.row, twin->cursor.col);
+
+        return pbar;
+}
+
+void DeletePBAR(PBAR_WIN *pbar)
+{
+        wclear(pbar->box);
+        wrefresh(pbar->box);
+        delwin(pbar->progress);
+        delwin(pbar->text);
+        delwin(pbar->box);
+        free(pbar);
+}
+
+void SetProgressPercent(TREE_WIN *twin, PBAR_WIN *pbar, int val)
+{
+        int step = (val * pbar->progress_size) / 100;
+        
+        werase(pbar->progress);
+        while(step-- > 0){
+                waddch(pbar->progress, '#');
+        }
+        wrefresh(pbar->progress);
+        MoveCursorToRowCol(twin, twin->cursor.row, twin->cursor.col);
+}
+
+void *CopyFile(void *arg)
+{
+        struct mCopyInfo *mci = (struct mCopyInfo *)arg;
+        char pb_message[CURDIR_SIZE * 2];
+        int to_d;
+        int from_d;
+        char copy_buff[64];
+        off_t size;
+        int copyed = 0;
+        int copyed_byte;
+        
+        PBAR_WIN *pbar;
+        
+        from_d = open(mci->from, O_RDONLY);
+        if(from_d < 0){
+                /*
+                        PRINT ERROR
+                */
+                pthread_exit(0);
+        }
+        to_d = open(mci->to, O_CREAT | O_RDWR, mci->from_flags);
+        if(to_d < 0){
+                /*
+                        PRINT ERROR
+                */
+                close(from_d);
+                pthread_exit(0);
+        }
+        size = lseek(from_d, 0, SEEK_END);
+        lseek(from_d, 0, SEEK_SET);
+        sprintf(pb_message, "%s\ncopy to\n%s", mci->from, mci->to);
+        pbar = CreatePBAR(*mci->twin, -1, -1, pb_message);
+        while( (copyed_byte = read(from_d, copy_buff, 64)) > 0){
+                write(to_d, copy_buff, copyed_byte);
+                copyed += copyed_byte;
+                SetProgressPercent(*mci->twin, pbar, (copyed * 100) / size);
+                sleep(1);//Ожидание для наглядности
+        }
+        close(from_d);
+        close(to_d);
+        DeletePBAR(pbar);
+        free(mci);
+        pthread_exit(0);
+}
+
+int CopyFlags(char *fname)
+{
+        struct stat fstat;
+
+        if( stat(fname, &fstat) > -1 ){
+                return fstat.st_mode;
+        }
+
+        return S_IRWXU;
+}
+
+int InitCopyFile(TREE_WIN **from_win, TREE_WIN *to_win)
+{
+        pthread_t pth;
+        struct mCopyInfo *mci;
+        struct dirent *name_list = (*from_win)->name_list_d[(*from_win)->name_index];
+        struct stat from_stat;
+        
+        mci = malloc(sizeof(struct mCopyInfo));
+        if(mci == NULL)
+                return -1;
+        mci->twin = from_win;
+        sprintf(mci->from, "%s/%s", (*from_win)->cur_dir, name_list->d_name);
+        sprintf(mci->to, "%s/%s", to_win->cur_dir, name_list->d_name);
+        if(stat(mci->from, &from_stat) < -1){//Непонятный файл
+                free(mci);
+                return -1;
+        }
+        if( (from_stat.st_mode & S_IFMT) == S_IFDIR ){//Директория
+                free(mci);
+                return -1;
+        }
+        mci->from_flags = from_stat.st_mode;
+        pthread_create(&pth, NULL, CopyFile, mci);
+        
+        return 0;
+}
+
+int FileIsRead(struct stat *sf)
+{
+        if( sf->st_mode & S_IRUSR || sf->st_mode & S_IRGRP || sf->st_mode & S_IROTH )
+                return 0;
+        
+        return -1;
+}
+
+int FileIsWrite(struct stat *sf)
+{
+        if( sf->st_mode & S_IWUSR || sf->st_mode & S_IWGRP || sf->st_mode & S_IWOTH )
+                return 0;
+        
+        return -1;
+}
+
+int FileIsExe(struct stat *sf)
+{
+        if( sf->st_mode & S_IXUSR || sf->st_mode & S_IXGRP || sf->st_mode & S_IXOTH )
+                return 0;
+        
+        return -1;
 }
--- eltex/curs/tfm/tfm.h
+++ eltex/curs/tfm/tfm.h
@@ -9,13 +9,22 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <pthread.h>
+#include <fcntl.h>
 
 #define TREE_WIN struct WindowTree
 #define MCURS struct mCursor
+#define PBAR_WIN struct mProgressBar
+
+#define CURDIR_SIZE 256
+/*
+        row - y - height
+        col - x - width
+*/
 
 MCURS {
-        off_t x_row;
-        off_t y_col;
+        off_t row;
+        off_t col;
 };
 
 TREE_WIN {
@@ -29,7 +38,22 @@ TREE_WIN {
         off_t nl_rows;
         int name_index;
         int name_list_d_size;
+        char cur_dir[CURDIR_SIZE];
         struct dirent **name_list_d;
+};
+
+PBAR_WIN {
+        WINDOW *box;
+        WINDOW *text;
+        WINDOW *progress;
+        int progress_size;
+};
+
+struct mCopyInfo {
+        TREE_WIN **twin;
+        char from[CURDIR_SIZE];
+        char to[CURDIR_SIZE];
+        int from_flags;
 };
 
 void GetWinSize(int *, int *);
@@ -45,3 +69,12 @@ int OnPressEnter(TREE_WIN *);
 int StartFile(char *);
 int EditFile(char *);
 TREE_WIN *SwitchWin(TREE_WIN *, TREE_WIN *, TREE_WIN *);
+PBAR_WIN *CreatePBAR(TREE_WIN *, int, int, char *);
+void SetProgressPercent(TREE_WIN *, PBAR_WIN *, int);
+int ChDir(TREE_WIN *, char *);
+int ChDir_NotMove(TREE_WIN *, char *);
+void *CopyFile(void *);
+int InitCopyFile(TREE_WIN **, TREE_WIN *);
+int FileIsRead(struct stat *);
+int FileIsWrite(struct stat *);
+int FileIsExe(struct stat *);
